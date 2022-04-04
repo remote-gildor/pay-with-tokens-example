977fa2f15cf38f901b9c08f59f6767a3432b64c0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-04 17:01:08 +0800

    Delete migrations directory

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const BasicToken = artifacts.require("BasicToken");
-const SomeService = artifacts.require("SomeService");
-const TokenWithPermit = artifacts.require("TokenWithPermit");
-
-module.exports = function(deployer, network, accounts) {
-  deployer.deploy(BasicToken);
-  deployer.deploy(TokenWithPermit, accounts[1], accounts[0]); // both accounts should get tokens
-  deployer.deploy(SomeService);
-};
